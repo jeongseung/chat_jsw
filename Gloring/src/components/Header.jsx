c8f990e9d5282f693b06a3c958577007d3b738e2
정다훈 <dahoonjeong3572@gmail.com>
2025-07-03 09:47:41 +0900

    로고 포인터

--- Gloring/src/components/Header.jsx
+++ Gloring/src/components/Header.jsx
@@ -5,7 +5,13 @@ const Header = () => {
   return (
     <header className='header'>
       <div className='header-inner'>
-        <h1 className='logo'>Gloring</h1>
+       <h1
+  className="logo"
+  style={{ cursor: 'pointer' }} // 클릭 가능한 느낌을 주기 위해 커서 추가
+  onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+>
+  Gloring
+</h1>
         <nav className='nav'>
           <a href='#calculator' className='nav-btn'>손익 계산 시뮬레이터</a>
           <a href='#trend'className='nav-btn'>트렌드 인사이트</a>
