63d5263da9ebf2ebacf7b76870c9b1d4c2807d0a
JSW <tmddn1008@naver.com>
2025-07-08 07:14:47 +0900

    fix : vite.config.js ip수정

--- Gloring/vite.config.js
+++ Gloring/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    // 모든 IP 주소에서의 접속을 허용합니다.
+    host: '0.0.0.0' 
+  }
 })
