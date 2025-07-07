3d48be63acae7f2b0255204cafee52d88405bee1
JSW <tmddn1008@naver.com>
2025-07-07 18:48:24 +0900

    fix : tests파일 이름 수정

--- Gloring_back/src/test/java/kr/smhrd/navertest/GloringApplicationTests.java
+++ Gloring_back/src/test/java/kr/smhrd/navertest/GloringApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Bonlunch1ApplicationTests {
+class GloringApplicationTests {
 
 	@Test
 	void contextLoads() {
