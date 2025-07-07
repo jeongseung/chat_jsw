afb523bf1b9d0ce0e3635912c7e507fb2454208f
JSW <tmddn1008@naver.com>
2025-07-08 05:30:19 +0900

    fix : ip주소 변경

--- Gloring_back/src/main/java/com/smhrd/gloring/config/SecurityConfig.java
+++ Gloring_back/src/main/java/com/smhrd/gloring/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://127.0.0.1:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://43.201.67.86:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
