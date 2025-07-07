62234ac93234cbf06198e00f235c71b12d2b5098
김수연 <gim4561710@gmail.com>
2025-07-07 17:16:33 +0900

    계산기 수정 및 헤더 등등 수정

--- Gloring/src/components/Header.jsx
+++ Gloring/src/components/Header.jsx
@@ -25,11 +25,11 @@ const Header = ({ isLoggedIn, setIsLoggedIn }) => {
   return (
     <header className={`header ${scrollDirection === 'down' ? 'hide' : ''}`}>
       <div className='header-inner'>
-      <h7
+      <h1
       className="logo"
       style={{ cursor: 'pointer' }} // 클릭 가능한 느낌을 주기 위해 커서 추가
       onClick={goToHome}
-        >Gloring </h7>
+        >Gloring </h1>
         <nav className='nav'>
           <a href='#calculator' className='nav-btn'>순이익 계산 시뮬레이터</a>
           <a href='#trend'className='nav-btn'>트렌드 인사이트</a>
